bf2452e257ba556695e8c84fd6efdc17697311e9
wrewolf <wrewolf@gmail.com>
2014-10-20 11:32:03 +0400

    Update OnlineCount.php

--- OnlineCount.php
+++ OnlineCount.php
@@ -1,4 +1,5 @@
 <?php
+
   /*
 __PocketMine Plugin__
 name=OnlineCount
@@ -6,12 +7,12 @@ description=OnlineCount
 version=0.5
 author=WreWolf
 class=OnlineCount
-apiversion=9
+apiversion=12
 */
 
   class OnlineCount implements Plugin
   {
-    private $api, $path, $config, $server, $db;
+    private $api, $server,$mysqli;
 
     public function __construct(ServerAPI $api, $server = false)
     {
@@ -21,15 +22,16 @@ apiversion=9
 
     public function init()
     {
-      $this->path = $this->api->plugin->configPath($this);
-      @mkdir($this->path);
       $this->api->schedule(100, array($this, "tickHandler"), array(), true, "server.schedule");
-
-      if ($this->db = new SQLite3($this->path."main.db")) {
-        // first let the engine check table, and create it eventualy
-        $q = @$this->db->query('CREATE TABLE IF NOT EXISTS main (id INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL, "timestamp" NOT NULL DEFAULT CURRENT_TIMESTAMP, tps real, online int);');
-      }
+      //$this->api->schedule(10000, array($this, "cronHandler"), array(), true, "server.schedule");
+      $this->mysqli = new mysqli('127.0.0.1', 'root', null);
+      $this->mysqli->select_db("m1");
     }
+
+    //public function cronHandler($data, $event)
+    //{
+//
+    //}
 
     public function tickHandler($data, $event)
     {
@@ -37,32 +39,50 @@ apiversion=9
       $data['time']     = date("Y-m-d H:i:s");
       $data['online']   = "Online: " . count($this->api->player->getAll());
       $info             = $this->server->debugInfo();
-      $data['tps']      = "TPS: " . $info["tps"];
-      $data['mem']      = "Memory: " . $info["memory_usage"];
-      $data['peakmem']  = "Peak: " . $info["memory_peak_usage"];
-      $data['entities'] = "Entities: " . $info["entities"];
-      $data['players']  = "Players: " . $info["players"];
-      $data['events']   = "Events: " . $info["events"];
-      $data['handlers'] = "Handlers: " . $info["handlers"];
-      $data['actions']  = "Actions: " . $info["actions"];
-      $data['garbage']  = "Garbage cycles: " . $info["garbage"];
-      $data['users']    = array();
-      foreach ($this->server->clients as $c) {
-        $data['users'][] = array('name' => $c->username, 'x' => $c->entity->x, 'y' => $c->entity->y, 'z' => $c->entity->z, 'level' => $c->level->getName());
+      $data['tps']      = $info["tps"];
+      $data['mem']      = $info["memory_usage"];
+      $data['peakmem']  = $info["memory_peak_usage"];
+      $data['entities'] = $info["entities"];
+      $data['players']  = $info["players"];
+      $data['events']   = $info["events"];
+      $data['handlers'] = $info["handlers"];
+      $data['actions']  = $info["actions"];
+      $data['garbage']  = $info["garbage"];
+
+      $misc             = $this->mysqli->real_escape_string(json_encode($data));
+
+      $playersA = array();
+      $clients = $this->api->player->getAll();
+      foreach ($clients as $c) {
+        if ($c->username === "")
+          continue;
+          if($c instanceof Player)
+            $playersA[] = array('name' => $c->username, 'x' => $c->entity->x, 'y' => $c->entity->y, 'z' => $c->entity->z, 'level' => $c->level->getName());
       }
 
+      $online              = $this->mysqli->real_escape_string(json_encode($playersA));
       $ops                 = new Config(DATA_PATH . "ops.txt", CONFIG_LIST); //Open list of OPs
       $data['ops'][]       = $ops->getAll(true);
+      $ops                 = $this->mysqli->real_escape_string(json_encode($data['ops']));
       $banned              = new Config(DATA_PATH . "banned.txt", CONFIG_LIST); //Open Banned Usernames list file
       $data['banned'][]    = $banned->getAll(true);
+      $banned              = $this->mysqli->real_escape_string(json_encode($data['banned']));
       $bannedIPs           = new Config(DATA_PATH . "banned-ips.txt", CONFIG_LIST); //Open Banned IPs list file
       $data['bannedIPs'][] = $bannedIPs->getAll(true);
+      $bannedips           = $this->mysqli->real_escape_string(json_encode($data['bannedIPs']));
+      $misc_q              = "UPDATE misc SET ops='$ops', banned='$banned', bannedips='$bannedips', misc='$misc', online='$online' WHERE id=1;";
 
-      //console(print_r(json_encode($data), true));
-      file_put_contents($this->path . "counter.txt", json_encode($data));
       preg_match_all('!\d+\.*\d*!', $info['tps'], $matches);
-      $tps = $matches[0][0];
-      $this->db->query("INSERT INTO main (tps,online) VALUES ('$tps','{$info["players"]}')");
+      $tps   = $matches[0][0];
+      $time  = date('Y-m-d H:i:s');
+      $query = "INSERT INTO main (tps,online,timestamp) VALUES ('$tps','{$info["players"]}','$time');" . $misc_q;
+
+      foreach ($playersA as $key => $player) {
+        $query .= "INSERT INTO online (user,cnt,level,x,y,z,timestamp) VALUES ('{$player['name']}',0,'{$player['level']}','{$player['x']}','{$player['y']}','{$player['z']}','$time') ON DUPLICATE KEY UPDATE cnt = cnt+1,level='{$player['level']}',x='{$player['x']}',y='{$player['y']}',z='{$player['z']}',timestamp='$time';";
+      }
+
+      $data = array('query' => $query);
+      $this->api->handle("db.query.exec", $data);
     }
 
     public function __destruct()
